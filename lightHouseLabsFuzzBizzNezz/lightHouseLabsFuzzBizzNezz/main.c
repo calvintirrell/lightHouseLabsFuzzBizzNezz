9bcb83166a480de39f48dd171f77f6b4757864d3
Calvin Tirrell <Calvin@Calvins-MacBook-Pro.local>
2015-12-14 13:30:22 -0800

    added project

---
+++ lightHouseLabsFuzzBizzNezz/lightHouseLabsFuzzBizzNezz/main.c
@@ -0,0 +1,33 @@
+//  main.c
+//  lightHouseLabsFuzzBizzNezz
+//
+//  Created by Calvin Tirrell on 12/11/15.
+//  Copyright © 2015 CT. All rights reserved.
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    
+    int b, f, n, x;
+    
+    printf("Please enter an integer between 100 and 500, including 100 and 500: \n");
+    scanf("%d", &n);
+    printf("Please enter another integer that is between 1 and 10, including 10: \n");
+    scanf("%d", &f);
+    printf("Please enter a third integer that is between 1 and 10, including 10: \n");
+    scanf("%d", &b);
+    printf("\n");
+    
+    for (x = 1; x <= n; x++) {
+      if ((x%f == 0) && (x%b == 0)) {
+        printf("FuzzBizz\n");
+      } else if ((x%f == 0)) {
+        printf("Fuzz\n");
+      } else if ((x%b == 0)) {
+        printf("Bizz\n");
+      } else {
+        printf("%d\n",x);
+      }
+    }
+    return 0;
+}
