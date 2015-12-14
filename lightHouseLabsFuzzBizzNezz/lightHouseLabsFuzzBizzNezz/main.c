3e1970ff6efe3db090a6dd5307683e68b9f0b941
Calvin Tirrell <Calvin@Calvins-MacBook-Pro.local>
2015-12-14 14:13:18 -0800

    updated the project

--- lightHouseLabsFuzzBizzNezz/lightHouseLabsFuzzBizzNezz/main.c
+++ lightHouseLabsFuzzBizzNezz/lightHouseLabsFuzzBizzNezz/main.c
@@ -6,9 +6,23 @@
 
 #include <stdio.h>
 
+void FuzzBizz(int b, int f, int n) {
+    for (int x = 1; x <= n; x++) {
+        if ((x%f == 0) && (x%b == 0)) {
+            printf("FuzzBizz\n");
+        } else if ((x%f == 0)) {
+            printf("Fuzz\n");
+        } else if ((x%b == 0)) {
+            printf("Bizz\n");
+        } else {
+            printf("%d\n",x);
+        }
+    }
+}
+
 int main(int argc, const char * argv[]) {
     
-    int b, f, n, x;
+    int b, f, n;
     
     printf("Please enter an integer between 100 and 500, including 100 and 500: \n");
     scanf("%d", &n);
@@ -17,17 +31,6 @@ int main(int argc, const char * argv[]) {
     printf("Please enter a third integer that is between 1 and 10, including 10: \n");
     scanf("%d", &b);
     printf("\n");
-    
-    for (x = 1; x <= n; x++) {
-      if ((x%f == 0) && (x%b == 0)) {
-        printf("FuzzBizz\n");
-      } else if ((x%f == 0)) {
-        printf("Fuzz\n");
-      } else if ((x%b == 0)) {
-        printf("Bizz\n");
-      } else {
-        printf("%d\n",x);
-      }
-    }
+    FuzzBizz(b, f, n);
     return 0;
 }
